a68e147741b3878fed878c178a075bc6d612ffb6
Ravindu Indranath <indrana-ps21164@stu.kln.ac.lk>
2025-11-23 11:10:57 +0530

    create Entity classes

---
+++ src/main/java/com/Transpo/transpo/model/Route.java
@@ -0,0 +1,55 @@
+package com.Transpo.transpo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "routes")
+
+public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String origin;
+
+    @Column(nullable = false)
+    private String destination;
+
+    
+
+    public Route() {
+    }
+
+    public Route(String startLocation, String endLocation, String distance) {
+        this.origin = startLocation;
+        this.destination = endLocation;
+        
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
---
+++ src/main/java/com/Transpo/transpo/model/Schedule.java
@@ -0,0 +1,77 @@
+package com.Transpo.transpo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "schedules")
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Bus bus;
+    @ManyToOne(optional = false)
+    private Route route;
+
+    private LocalDateTime departurTime;
+
+    private double fare;
+
+    //available seats to book (keeps updated)
+    private int availableSeats;
+
+    public Schedule() {
+    }
+
+    public Schedule(Bus bus, Route route, LocalDateTime departurTime, double fare, int availableSeats) {
+        this.bus = bus;
+        this.route = route;
+        this.departurTime = departurTime;
+        this.fare = fare;
+        this.availableSeats = availableSeats;
+    }
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Bus getBus(){return bus; }
+    public void setBus(Bus bus){ this.bus = bus; }
+
+    public Route getRoute(){ return route; }
+    public void setRoute(Route route){ this.route = route; }
+
+    public LocalDateTime getDeparturTime() {
+        return departurTime;
+    }
+
+    public void setDeparturTime(LocalDateTime departurTime) {
+        this.departurTime = departurTime;
+    }
+
+    public double getFare() {
+        return fare;
+    }
+
+    public void setFare(double fare) {
+        this.fare = fare;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+}
